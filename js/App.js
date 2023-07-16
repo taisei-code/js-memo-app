d563a02d7b292e3c59e6756df8b655be98ccf830
Taisei <taisei96.web@gmail.com>
2023-07-16 18:16:15 +0900

    サイドバー

--- js/App.js
+++ js/App.js
@@ -1,10 +1,35 @@
 import NotesView from "./NotesView.js";
+import NotesAPI from "./NotesAPI.js";
 
 export default class App {
   constructor(root) {
     this.note = [];
     this.activeNote = null;
     this.view = new NotesView(root, this._handlers());
+
+    // 最初に必ず呼ばれる
+    this._refreshNotes();
+  }
+
+  _refreshNotes() {
+    const notes = NotesAPI.getAllNotes();
+    console.log(notes);
+
+    this._setNotes(notes);
+
+    if (notes.length > 0) {
+      this._setActiveNote(notes[0]);
+    }
+  }
+
+  _setNotes(notes) {
+    this.notes = notes;
+    this.view.updateNoteList(notes);
+  }
+
+  _setActiveNote(note) {
+    this.activeNote = note;
+    this.view.updateActiveNote(note);
   }
 
   // 処理群
